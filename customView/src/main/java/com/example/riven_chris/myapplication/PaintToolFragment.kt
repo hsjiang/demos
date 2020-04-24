28acfc9ad7144a979d3691a8e63c7035d1c76c2a
hsjiang <hsj7107713@163.com>
2020-04-24 16:28:17 +0800

    commit

--- customView/src/main/java/com/example/riven_chris/myapplication/PaintToolFragment.kt
+++ customView/src/main/java/com/example/riven_chris/myapplication/PaintToolFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_paint_tool.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -34,6 +35,13 @@ class PaintToolFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_paint_tool, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        btnUndo.setOnClickListener {
+            toolView.unDo()
+        }
     }
 
     companion object {
--- customView/src/main/java/views/PaintToolView.kt
+++ customView/src/main/java/views/PaintToolView.kt
@@ -1,19 +1,23 @@
 package views
 
 import android.content.Context
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.Paint
-import android.graphics.Path
+import android.graphics.*
 import android.util.AttributeSet
 import android.util.Log
 import android.view.MotionEvent
 import android.view.View
+import androidx.annotation.ColorInt
 import kotlin.math.abs
 
 val TAG_EVENT_ACTION = "event_action"
+val TAG_CAVANS_DRAW = "cavans_draw"
 
 class PaintToolView : View {
+    enum class Mode {
+        LINE,
+        ARROW,
+        ERASER
+    }
 
     private val INVALID_POINTER = -1
     private val MIN_MOVE_DIS = 5
@@ -24,8 +28,12 @@ class PaintToolView : View {
     private var mLastTouchX = 0f
     private var mLastTouchY = 0f
 
-    private var mPaint = Paint(Paint.ANTI_ALIAS_FLAG)
-    private var mPath: Path? = null
+    private var mDrawShape: DrawShape? = null
+    private val mCanvas: Canvas = Canvas()
+    private var mBitmap: Bitmap? = null
+    private var mColor: Int = Color.RED
+
+    private var mPathList = arrayListOf<DrawShape>()
 
     constructor(context: Context) : super(context)
     constructor(context: Context, attributeSet: AttributeSet?) : super(context, attributeSet)
@@ -36,17 +44,18 @@ class PaintToolView : View {
     )
 
     init {
-        mPaint.color = Color.GREEN
-        mPaint.style = Paint.Style.STROKE
-        mPaint.strokeWidth = 15f
-        mPaint.strokeCap = Paint.Cap.ROUND
-        mPaint.strokeJoin = Paint.Join.ROUND
+        addOnLayoutChangeListener { v, left, top, right, bottom, oldLeft, oldTop, oldRight, oldBottom ->
+            if (left != oldLeft || top != oldTop || right != oldRight || bottom != oldBottom) {
+                mBitmap = Bitmap.createBitmap(right - left, bottom - top, Bitmap.Config.ARGB_8888)
+                mCanvas.setBitmap(mBitmap)
+                invalidate()
+            }
+        }
     }
 
     override fun onTouchEvent(event: MotionEvent?): Boolean {
         event?.apply {
             val actionIndex: Int = actionIndex
-
             when (action) {
                 MotionEvent.ACTION_DOWN -> {
                     mPointerId = getPointerId(0)
@@ -54,20 +63,22 @@ class PaintToolView : View {
                     mInitialTouchY = y
                     mLastTouchX = x
                     mLastTouchY = y
+                    onDown(x, y)
                     Log.d(TAG_EVENT_ACTION, "ACTION_DOWN mPointerId:${mPointerId}")
                 }
                 MotionEvent.ACTION_POINTER_DOWN -> {
                     mPointerId = getPointerId(actionIndex)
                     mLastTouchX = getX(actionIndex)
                     mLastTouchY = getY(actionIndex)
+                    onDown(mLastTouchX, mLastTouchY)
                     Log.d(TAG_EVENT_ACTION, "ACTION_POINTER_DOWN mPointerId:${mPointerId}")
                 }
                 MotionEvent.ACTION_MOVE -> {
                     val index: Int = findPointerIndex(mPointerId)
-                    if (index > 0) return true
+                    if (index < 0) return true
 
                     if (abs(x - mLastTouchX) >= MIN_MOVE_DIS || abs(y - mLastTouchY) >= MIN_MOVE_DIS) {
-                        drawPath(x, y)
+                        onMove(x, y)
                         mLastTouchX = x
                         mLastTouchY = y
                         Log.d(TAG_EVENT_ACTION, "ACTION_MOVE mPointerId:${mPointerId}")
@@ -80,14 +91,15 @@ class PaintToolView : View {
                         mPointerId = getPointerId(newIndex)
                         mLastTouchX = getX(newIndex)
                         mLastTouchY = getY(newIndex)
+                        onDown(mLastTouchX, mLastTouchY)
                     }
                     Log.d(TAG_EVENT_ACTION, "ACTION_POINTER_UP mPointerId:${mPointerId}")
                 }
                 MotionEvent.ACTION_UP,
                 MotionEvent.ACTION_CANCEL -> {
-                    mPath == null
-                    val mPointerId = getPointerId(actionIndex)
-                    Log.d(TAG_EVENT_ACTION, "ACTION_UP mPointerId:${mPointerId}")
+                    onUp()
+//                    val mPointerId = getPointerId(actionIndex)
+//                    Log.d(TAG_EVENT_ACTION, "ACTION_UP mPointerId:${mPointerId}")
                 }
                 else -> return true
             }
@@ -95,21 +107,150 @@ class PaintToolView : View {
         return true
     }
 
-    override fun onDraw(canvas: Canvas?) {
-        super.onDraw(canvas)
+    fun unDo() {
+        if (mPathList.isEmpty()) return
+        mBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
+        mCanvas.setBitmap(mBitmap)
 
-        mPath?.let {
-            canvas?.drawPath(it, mPaint)
-        }
-    }
-
-    private fun drawPath(x: Float, y: Float) {
-        if (mPath == null) {
-            mPath = Path()
-            mPath?.moveTo(x, y)
-        } else {
-            mPath?.quadTo(mLastTouchX, mLastTouchY, (x + mLastTouchX) / 2, (y + mLastTouchY) / 2)
+        mPathList.removeAt(mPathList.size - 1)
+        mPathList.forEach {
+            it.draw(mCanvas)
         }
         invalidate()
     }
+
+    fun getBitmap(): Bitmap? {
+        return mBitmap
+    }
+
+    fun setColor(@ColorInt color: Int) {
+        mColor = color
+        mDrawShape?.changeColor(mColor)
+    }
+
+    override fun onDraw(canvas: Canvas?) {
+        mBitmap?.also { canvas?.drawBitmap(it, 0f, 0f, null) }
+    }
+
+    private fun onMove(x: Float, y: Float) {
+        mDrawShape?.move(mLastTouchX, mLastTouchY, x, y)
+        mDrawShape?.draw(mCanvas)
+        invalidate()
+    }
+
+    private fun onDown(x: Float, y: Float) {
+        if (mDrawShape == null) {
+            mDrawShape = PathShape(mColor)
+        }
+        mDrawShape?.down(x, y)
+        mDrawShape?.draw(mCanvas)
+        invalidate()
+    }
+
+    private fun onUp() {
+        mDrawShape?.also { mPathList.add(it) }
+        mDrawShape = null
+    }
+
+    private interface DrawShape {
+        fun down(x: Float, y: Float)
+        fun move(x1: Float, y1: Float, x2: Float, y2: Float)
+        fun up(x: Float, y: Float){}
+        fun draw(canvas: Canvas)
+        fun changeColor(@ColorInt color: Int){}
+    }
+
+    private class PathShape(@ColorInt color: Int) : DrawShape {
+        private val paint: Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+        private val path: Path = Path()
+
+        init {
+            paint.color = color
+            paint.style = Paint.Style.STROKE
+            paint.strokeWidth = 15f
+            paint.strokeCap = Paint.Cap.ROUND
+            paint.strokeJoin = Paint.Join.ROUND
+        }
+
+        override fun down(x: Float, y: Float) {
+            path.moveTo(x, y)
+        }
+
+        override fun move(x1: Float, y1: Float, x2: Float, y2: Float) {
+            path.quadTo(x1, y1, (x2 + x1) / 2, (y2 + y1) / 2)
+        }
+
+        override fun draw(canvas: Canvas) {
+            canvas.drawPath(path, paint)
+        }
+
+        override fun changeColor(color: Int) {
+            paint.color = color
+        }
+    }
+
+    private class ArrowShape(@ColorInt color: Int) : DrawShape {
+        private val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+        private val rectF = RectF(0f, 0f, 0f, 0f)
+        private val trianglePath = Path()
+
+        override fun down(x: Float, y: Float) {
+            TODO("Not yet implemented")
+        }
+
+        override fun move(x1: Float, y1: Float, x2: Float, y2: Float) {
+            TODO("Not yet implemented")
+        }
+
+        override fun up(x: Float, y: Float) {
+            TODO("Not yet implemented")
+        }
+
+        override fun draw(canvas: Canvas) {
+
+        }
+
+        override fun changeColor(color: Int) {
+            TODO("Not yet implemented")
+        }
+    }
+
+//    private fun getArrow(sx: Float, sy: Float, ex: Float, ey: Float, width: Int): Path {
+//        var size = 5
+//        var count = 20
+//        when (width) {
+//            0 -> {
+//                size = 5
+//                count = 20
+//            }
+//            5 -> {
+//                size = 8
+//                count = 30
+//            }
+//            10 -> {
+//                size = 11
+//                count = 40
+//            }
+//        }
+//        var x = ex - sx
+//        var y = ey - sy
+//        var d = (x * x + y * y).toDouble()
+//        var r = sqrt(d)
+//        val zx = (ex - (count * x / r)).toFloat()
+//        val zy = (ey - (count * y / r)).toFloat()
+//        val xz = zx - sx
+//        val yz = zy - sy
+//        val zd = (xz * xz + yz * yz).toDouble()
+//        val zr = sqrt(zd)
+//        val triangle = Path()
+//        triangle.moveTo(sx, sy)
+//        triangle.lineTo((zx + size * yz / zr).toFloat(), (zy - size * xz / zr).toFloat())
+//        triangle.lineTo((zx + size * 2 * yz / zr).toFloat(), (zy - size * 2 * xz / zr).toFloat())
+//        triangle.lineTo(ex, ey)
+//        triangle.lineTo((zx - size * 2 * yz / zr).toFloat(), (zy + size * 2 * xz / zr).toFloat())
+//        triangle.lineTo((zx - size * yz / zr).toFloat(), (zy + size * xz / zr).toFloat())
+//        triangle.close()
+//
+//        return triangle
+//    }
 }
