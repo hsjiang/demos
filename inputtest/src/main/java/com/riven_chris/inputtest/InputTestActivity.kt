4fe7a5d963ed35664493ccaee65fb76ff0488d56
hsjiang <hsj7107713@163.com>
2020-06-29 17:46:02 +0800

    commit

---
+++ inputtest/src/main/java/com/riven_chris/inputtest/InputTestActivity.kt
@@ -0,0 +1,12 @@
+package com.riven_chris.inputtest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class InputTestActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_input_test)
+    }
+}
